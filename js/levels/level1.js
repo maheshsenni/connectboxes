ce3d74a65e639b43cbd26f0d70ea8e420c678eb7
Mahesh <mahesh.h4evr@gmail.com>
2013-11-18 20:33:03 +0530

    New Level 1

--- js/levels/level1.js
+++ js/levels/level1.js
@@ -1,18 +1,6 @@
 var level = [{
-	'cell1': 'rect-0-0',
-	'cell2': 'rect-9-9',
-	'connected': false,
-	'color': 'red'
-},
-{
-	'cell1': 'rect-1-1',
-	'cell2': 'rect-8-8',
-	'connected': false,
-	'color': 'blue'
-},
-{
-	'cell1': 'rect-2-2',
-	'cell2': 'rect-7-7',
+	'cell1': 'rect-2-4',
+	'cell2': 'rect-7-5',
 	'connected': false,
 	'color': 'green'
 },
@@ -23,8 +11,8 @@ var level = [{
 	'color': 'orange'
 },
 {
-	'cell1': 'rect-4-4',
-	'cell2': 'rect-5-5',
+	'cell1': 'rect-5-4',
+	'cell2': 'rect-4-5',
 	'connected': false,
 	'color': 'purple'
 }];
